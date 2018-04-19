1685e7ee26f0881b50b7810148ac4563c0fbe105
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-04-19 09:15:00 +0900

    edit button to improve design

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'searches/index'
+
+  get 'searchs/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
   resources :curriculums
   resources :users
   resources :skills
+  resources :searchs
 end
